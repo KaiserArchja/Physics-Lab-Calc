b47f4d5b0d7bc18929bbf7ac03c4762afdbd6567
Archja <arshiyanorpour@gmail.com>
2025-02-24 18:02:55 +0330

    I wrote Least Square signature!

---
+++ include/funcs.hpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+std::map<std::string, double> LeastSquare(std::vector<double> x, std::vector<double> y);
---
+++ src/funcs.cxx
@@ -0,0 +1,6 @@
+#include "funcs.hpp"
+
+
+std::map<std::string, double> LeastSquare(std::vector<double> x, std::vector<double> y){
+    
+}
