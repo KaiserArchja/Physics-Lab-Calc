87fc98d5d4c94a9b10d9d3344ff1010f87f73f32
Archja <arshiyanorpour@gmail.com>
2025-02-24 18:48:36 +0330

    A bit changes!

--- include/funcs.hpp
+++ include/funcs.hpp
@@ -2,5 +2,7 @@
 #include <map>
 #include <cmath>
 #include <string>
+#include <iostream>
 
 std::map<std::string, double> LeastSquare(std::vector<double> x, std::vector<double> y);
+void LeastSquare();
